e3a02a14675016127e748d34414a255a1a9d06ef
djbarrow314 <djbarrow@byui.edu>
2021-05-20 14:09:16 -0400

    DonJon's Commit

--- src/ponce/Main.java
+++ src/ponce/Main.java
@@ -3,6 +3,6 @@ package ponce;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hello, my name is Jonathan");
+	System.out.print("Hello, my name is Jonathan\n");
 	System.out.print("Hello Jonathan, my name is DonJon");    }
 }
